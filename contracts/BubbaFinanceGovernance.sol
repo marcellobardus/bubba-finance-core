da6ff868e0963127ea9ba9569f825f101cf2bd54
Marcello Bardus <marcellobardus8@gmail.com>
2020-10-11 21:54:23 +0200

    governance touched

---
+++ contracts/BubbaFinanceGovernance.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.6.2;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+contract BubbaFinanceGovernance is ERC20 {
+    using SafeMath for uint256;
+}
