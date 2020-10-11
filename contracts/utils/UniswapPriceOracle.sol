769d053e3d5d060f75afbe7c69a0e9f633b5bbe4
Marcello Bardus <marcellobardus8@gmail.com>
2020-10-11 13:46:10 +0200

    initial commit

---
+++ contracts/utils/UniswapPriceOracle.sol
@@ -0,0 +1,14 @@
+pragma solidity ^0.6.2;
+
+import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
+
+library UniswapPriceOracle {
+    using SafeMath for uint256;
+
+    function getPrice(IUniswapV2Pair pair) public view returns (uint256) {
+        (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast) = pair
+            .getReserves();
+        return uint256(reserve0).div(uint256(reserve1));
+    }
+}
